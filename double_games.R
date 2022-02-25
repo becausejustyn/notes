863b6136a7d6693ee99d5c4fb30e9b4b9b96a46c
becausejustyn <77762513+becausejustyn@users.noreply.github.com>
2022-02-25 16:30:25 +1100

    double for plotting

---
+++ double_games.R
@@ -0,0 +1,25 @@
+# double games
+## takes input where each game has one row with teams as `away_team` and `home_team`
+## returns with each game having two rows with teams listed as `team` and `opp`
+double_games <- function(g)
+{
+  g1 <- g %>% 
+    rename(team=away_team,team_score=away_score,
+           opp=home_team,opp_score=home_score,
+           team_rest=away_rest,opp_rest=home_rest,
+           team_moneyline=away_moneyline,opp_moneyline=home_moneyline,
+           team_spread_odds=away_spread_odds,opp_spread_odds=home_spread_odds,
+           team_coach=away_coach,opp_coach=home_coach) %>% 
+    mutate(location=ifelse(location == "Home","Away",location),
+           result=-1*result,spread_line=-1*spread_line)
+  g2 <- g %>% 
+    rename(team=home_team,team_score=home_score,
+           opp=away_team,opp_score=away_score,
+           team_rest=home_rest,opp_rest=away_rest,
+           team_moneyline=home_moneyline,opp_moneyline=away_moneyline,
+           team_spread_odds=home_spread_odds,opp_spread_odds=away_spread_odds,           
+           team_coach=home_coach,opp_coach=away_coach)
+  g <- bind_rows(g1,g2) %>% 
+    arrange(gameday,gametime,old_game_id,location)
+  return(g)
+}
