e22e439fe485e8240c3dfb8e117755e84c0077d4
becausejustyn <77762513+becausejustyn@users.noreply.github.com>
2022-02-25 16:35:05 +1100

    more functions

--- double_games.R
+++ double_games.R
@@ -23,3 +23,35 @@ double_games <- function(g)
     arrange(gameday,gametime,old_game_id,location)
   return(g)
 }
+
+
+# smaller version
+double_games2 <- function(g) {
+  g1 <- g %>%
+    rename(
+      team = away_team, 
+      team_score = away_score,
+      opp = home_team, 
+      opp_score = home_score,
+      team_name = away_team.y, 
+      team_conf = away_conf,
+      team_div = away_div
+    ) %>%
+    mutate(
+      location = ifelse(location == "Home", "Away", location),
+      result = -1 * result, spread_line = -1 * spread_line
+    )
+  g2 <- g %>%
+    rename(
+      team = home_team, 
+      team_score = home_score,
+      opp = away_team, 
+      opp_score = away_score,
+      team_name = home_team.y, 
+      team_conf = home_conf,
+      team_div = home_div
+    )
+  g <- bind_rows(g1, g2) %>%
+    arrange(game_date, game_id, location)
+  return(g)
+}
---
+++ harmonic_mean.R
@@ -0,0 +1,3 @@
+harmonic_mean <- function(x, na.rm = TRUE){
+  1 / mean(1 / x)
+}
